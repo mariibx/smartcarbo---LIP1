4fa58d389b4e4de3a63c5a4e33ee5c05c26a6e9d
mariibx <rm99667@estudante.fieb.edu.br>
2026-09-14 11:03:42 -0300

    criaçao backend

---
+++ smartcarbo/src/main/java/com/smartcarbo/smartcarbo/SmartcarboApplication.java
@@ -0,0 +1,13 @@
+package com.smartcarbo.smartcarbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcarboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcarboApplication.class, args);
+	}
+
+}
---
+++ smartcarbo/src/test/java/com/smartcarbo/smartcarbo/SmartcarboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcarbo.smartcarbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcarboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
